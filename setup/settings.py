4622066c86cc2c3f7b4630fd6d920151b3314e75
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-15 05:43:55 -0300

    Confugurando o host

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l_(^#xr-x6jy)ip-gwr0)dw86d01khr^*thj$muc18x0f7w6&+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','receitas-da-marisa.herokuapp.com']
 
 
 # Application definition
