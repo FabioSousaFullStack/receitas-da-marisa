d51df07fcf39d06628fa87cacc6b6343d1b90fe2
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-17 08:16:22 -0300

    Retirando breve_descrição

--- receitas/models.py
+++ receitas/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Receita(models.Model):
     nome = models.CharField(max_length=255)
-    breve_descricao = models.TextField()
     ingredientes = models.TextField()
     modo_de_preparo = models.TextField()
     preco = models.DecimalField(decimal_places=2, max_digits=18)
